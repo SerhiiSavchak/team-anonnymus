a33e89184814e45bb41b13d2024e56d1220c3fa1
Valerii Arlamovskyi <valera.arl96@gmail.com>
2023-06-10 23:19:18 +0200

    зміни

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -26,16 +26,18 @@ switchButton.addEventListener('click', function() {
     openMenuBtn: document.querySelector("[data-menu-open]"),
     closeMenuBtn: document.querySelector("[data-menu-close]"),
     menu: document.querySelector("[data-menu]"),
-    body: document.querySelector("body"),
-
-
+    
   };
 
-  refs.openMenuBtn.addEventListener("click", toggleMenu);
-  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  refs.openMenuBtn.addEventListener("click", openMenu);
+  refs.closeMenuBtn.addEventListener("click", closeMenu);
 
-  function toggleMenu() {
-    refs.menu.classList.toggle("is-hidden");
-    refs.body.classList.toggle("no-scroll");
+  function openMenu() {
+    refs.menu.removeAttribute("hidden");
+  }
+
+  function closeMenu() {
+    refs.menu.setAttribute("hidden", true);
   }
 })();
+
