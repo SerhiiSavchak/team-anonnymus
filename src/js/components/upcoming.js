40bb41fe64734869e9d40222eb843789c6f3b88d
Roman Gorobets <Gorobetcrv@gmail.com>
2023-06-12 00:08:32 +0300

    corrections made

--- src/js/components/upcoming.js
+++ src/js/components/upcoming.js
@@ -5,8 +5,7 @@ const END_POINT_GENRE = 'genre/movie/list?language=en';
 const API_KEY = 'api_key=bd0a4499e3f0b036025d12595397227a';
 
 
-const upcomingList = document.querySelector('.upcoming-content')
-const addLibrary = document.querySelector('.js-add-library')
+const upcomingList = document.querySelector('.upcoming-response-content-js')
 
 const options = {
   method: 'GET',
@@ -45,8 +44,7 @@ async function fetchMovieDetails() {
   const movie = await fetchData();
   const genreData = await fetchGenres();
 
-  movie.genres = movie.genre_ids.map(id => genreData.find(genre => genre.id === id).name).join(', ');
-
+  movie.genres = movie.genre_ids.map(id => genreData.find(genre => genre.id === id).name).slice(0, 2).join(', ');
 
   return movie;
 }
@@ -59,15 +57,63 @@ function createMarkup(movie) {
   }
   const message = `<h3 class="upcoming-alert">Ops! No have any upcoming films...</h3>`;
   let filmContent = `
-  <div class ="wrap">
+  <div class ="upcoming-section-wrap">
   
     <picture class="upcoming-poster">
-        <source media="(min-width:768px)" srcset="https://image.tmdb.org/t/p/original${movie.backdrop_path}"/>
-        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="poster_path" style="width:auto;"/>
+        <source media="(min-width:768px)" srcset="https://image.tmdb.org/t/p/original${movie.backdrop_path}">
+        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="poster_path" style="width:auto;">
     </picture>
 
     <div>
+        <h3 class="upcoming-film__title">${movie.title}</h3>
+        
+        <div class ="info-film">
+            <div class="info-film-block">
+              <div class="info-film-item">
+                <p>Release date</p>
+                <p class="info-film__release-date">${(movie.release_date).replace(/^(\d+)-(\d+)-(\d+)$/, `$3.$2.$1`)}</p>
+              </div>
+
+              <div class="info-film-item">
+                <p>Vote / Votes</p>
+                <p class="info-film__votes-count"><span>${movie.vote_average}</span><b>/</b><span>${movie.vote_count}</span></p>
+              </div>
+            </div>
+            
+            <div class="info-film-block">
+              <div class="info-film-item">
+                <p>Popularity</p>
+                <p class="info-film__popularity text-color-white">${(Number(movie.popularity.toFixed(1)))}</p>
+              </div>
+
+              <div class="info-film-item">
+                <p>Genre</p>
+                <p class="info-film__genre-style text-color-white">${movie.genres}</p>
+              </div>
+            </div> 
+             
+        </div>
     
+  
+      <h3 class="film-description-title">ABOUT</h3>
+      <p class="film-description">${textContent}</p>
+      <button class="btn">add to my library</button>
+    </div>
+  </div>`;
+
+  return !movie.results ? filmContent : message;
+}
+
+window.addEventListener('load', onPageLoad);
+
+function onPageLoad() {
+  fetchMovieDetails().then(movie => {
+    upcomingList.insertAdjacentHTML('beforeend', createMarkup(movie));
+  });
+}
+
+
+{/* <div>
         <h3 class="upcoming-film-title">${movie.title}</h3>
         
         <div class ="info-film">
@@ -79,9 +125,7 @@ function createMarkup(movie) {
 
               <div class="info-film-item">
                 <p>Vote / Votes</p>
-                  <p class="votes-count">
-                    <span>${movie.vote_average}</span><b>/</b><span>${movie.vote_count}</span>
-                  </p>
+                <p class="votes-count"><span>${movie.vote_average}</span><b>/</b><span>${movie.vote_count}</span></p>
               </div>
             </div>
             
@@ -106,16 +150,5 @@ function createMarkup(movie) {
     </div>
   </div>`;
 
-  return movie ? filmContent : message;
-}
 
-window.addEventListener('load', onPageLoad);
-
-function onPageLoad() {
-  fetchMovieDetails().then(movie => {
-    upcomingList.insertAdjacentHTML('beforeend', createMarkup(movie));
-  });
-}
-
-
-
+ */}
