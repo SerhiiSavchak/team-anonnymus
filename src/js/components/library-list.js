2c7e6fa52258b812f9286039c17fa9c83c00517a
JoeShvartsev <120992324+JoeShvartsev@users.noreply.github.com>
2023-06-12 21:24:30 +0300

    library-list
    то что есть без жанров

--- src/js/components/library-list.js
+++ src/js/components/library-list.js
@@ -17,7 +17,7 @@ async function fetchMovieDetails(movieID) {
   try {
     const response = await fetch(`${BASE_URL}movie/${movieID}?language=en-US`, options);
     const data = await response.json();
-    return data.results;
+    return data;
   } catch (error) {
     throw error;
   }
@@ -27,16 +27,19 @@ async function fetchMovieDetails(movieID) {
 
 function getMoviesFromLocalStorage() {
   let moviesID = JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
+  console.log(moviesID)
   if (moviesID.length === 0) {
     renderEmptyLocalMarkup();
     console.log(moviesID);
   }
   return moviesID;
+  
 }
+// сохраняем результат функции(масив ID) в переменную для дальнейшей работы
 
 const moviesID = getMoviesFromLocalStorage();
 
-// Функция перебирает массив идентификаторов фильмов и создает массив промисов
+// Функция перебирает массив идентификаторов фильмов и создает массив промисов 
 
 async function renderFetch(moviesID) {
   try {
@@ -54,12 +57,86 @@ async function renderFetch(moviesID) {
 renderFetch(moviesID)
   .then(results => {
     console.log(results);
+    renderData(results)
   })
   .catch(error => {
     console.error('Error fetching movie details:', error);
   });
 
 
+
+// функция рендера разметки
+
+
+
+function renderData(movieItems) {
+  const container = document.getElementById('movies-container');
+
+  const movieItem = movieItems.map(movie => {
+    const {
+      poster_path,
+      release_date,
+      title,
+      vote_average,
+      genre_ids
+    } = movie;
+
+    // const genres = getGenreNamesByIds(genre_ids);
+
+    // const releaseYear = release_date.slice(0, 4);
+    return `
+      <li class="library-list-item">
+        <a class="library-item-link" href=""> 
+          <img class="library-list-img" src="https://image.tmdb.org/t/p/w500${poster_path}" alt="${title}" />
+          <div class="library-item-desc">
+            <div class="title-genre-date">
+              <p class="library-item-title">${title}</p>
+              // тут жанры
+            </div>
+          </div>
+          <div class="library-item-rating">
+            <div class="rating_value">${vote_average}</div>
+            <div class="rating-body">
+              <div class="rating__active" data-rating="${vote_average}">
+                <div class="rating__items">
+                  <input type="radio" class="rating__item" value="1" name="rating">
+                  <input type="radio" class="rating__item" value="2" name="rating">
+                  <input type="radio" class="rating__item" value="3" name="rating">
+                  <input type="radio" class="rating__item" value="4" name="rating">
+                  <input type="radio" class="rating__item" value="5" name="rating">
+                </div>
+              </div>
+            </div>
+          </div>
+        </a>
+      </li>
+    `;
+  });
+
+  container.insertAdjacentHTML('beforeend', movieItem.join(''));
+
+  const ratingActiveElements = document.querySelectorAll('.rating__active');
+  ratingActiveElements.forEach(ratingActiveElement => {
+    const rating = ratingActiveElement.dataset.rating;
+    const widthPercentage = (rating / 10) * 100;
+
+    ratingActiveElement.style.width = `${widthPercentage}%`;
+  });
+}
+
+// вот разметка под жанры
+
+/* <p class="library-item-genre-date"> */
+//                 ${genres.join(', ')} | 
+//                 <span class="library-item-date">
+//                   ${releaseYear}
+//                 </span> 
+//               </p>
+
+
+
+
+// это функция под жанры
 
 
 
@@ -96,68 +173,22 @@ renderFetch(moviesID)
 // }
 
 
-// function renderData() {
-//   const container = document.getElementById('movies-container');
-//   const moviesFromLocalStorage = getMoviesFromLocalStorage(); 
 
-//   const movieItems = moviesFromLocalStorage.map(movie => {
-//     const {
-//       poster_path,
-//       release_date,
-//       title,
-//       vote_average,
-//       genre_ids
-//     } = movie;
 
-//     const genres = getGenreNamesByIds(genre_ids);
 
-//     const releaseYear = release_date.slice(0, 4);
 
-//     return `
-//       <li class="library-list-item">
-//         <a class="library-item-link" href=""> 
-//           <img class="library-list-img" src="https://image.tmdb.org/t/p/w500${poster_path}" alt="${title}" />
-//           <div class="library-item-desc">
-//             <div class="title-genre-date">
-//               <p class="library-item-title">${title}</p>
-//               <p class="library-item-genre-date">
-//                 ${genres.join(', ')} | 
-//                 <span class="library-item-date">
-//                   ${releaseYear}
-//                 </span> 
-//               </p>
-//             </div>
-//           </div>
-//           <div class="library-item-rating">
-//             <div class="rating_value">${vote_average}</div>
-//             <div class="rating-body">
-//               <div class="rating__active" data-rating="${vote_average}">
-//                 <div class="rating__items">
-//                   <input type="radio" class="rating__item" value="1" name="rating">
-//                   <input type="radio" class="rating__item" value="2" name="rating">
-//                   <input type="radio" class="rating__item" value="3" name="rating">
-//                   <input type="radio" class="rating__item" value="4" name="rating">
-//                   <input type="radio" class="rating__item" value="5" name="rating">
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//         </a>
-//       </li>
-//     `;
-//   });
 
-//   container.insertAdjacentHTML('beforeend', movieItems.join(''));
 
-//   const ratingActiveElements = document.querySelectorAll('.rating__active');
-//   ratingActiveElements.forEach(ratingActiveElement => {
-//     const rating = ratingActiveElement.dataset.rating;
-//     const widthPercentage = (rating / 10) * 100;
 
-//     ratingActiveElement.style.width = `${widthPercentage}%`;
-//   });
-// }
-// renderData();
+
+
+
+
+// Это код для работы с полной информацией в локал сторидж, тут жанры работыли
+
+
+
+
 
 
 
