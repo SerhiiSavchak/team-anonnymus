cfe195d530942017913c96f893a517c2bf6ed6c9
X-RAY <intelekt200012@gmail.com>
2023-06-12 19:54:52 +0300

    add fix to section

--- src/js/components/upcoming.js
+++ src/js/components/upcoming.js
@@ -1,11 +1,9 @@
-
 const BASE_URL = 'https://api.themoviedb.org/3/';
 const END_POINT_UPCOMING = 'movie/upcoming';
 const END_POINT_GENRE = 'genre/movie/list?language=en';
 const API_KEY = 'api_key=bd0a4499e3f0b036025d12595397227a';
 
-
-const upcomingList = document.querySelector('.upcoming-response-content-js')
+const upcomingList = document.querySelector('.upcoming-response-content-js');
 
 const options = {
   method: 'GET',
@@ -27,7 +25,6 @@ async function fetchGenres() {
   }
 }
 
-
 async function fetchData() {
   try {
     const response = await fetch(`${BASE_URL}${END_POINT_UPCOMING}`, options);
@@ -39,16 +36,17 @@ async function fetchData() {
   }
 }
 
-
 async function fetchMovieDetails() {
   const movie = await fetchData();
   const genreData = await fetchGenres();
 
-  movie.genres = movie.genre_ids.map(id => genreData.find(genre => genre.id === id).name).slice(0, 2).join(', ');
+  movie.genres = movie.genre_ids
+    .map(id => genreData.find(genre => genre.id === id).name)
+    .slice(0, 2)
+    .join(', ');
 
   return movie;
 }
-
 
 function createMarkup(movie) {
   let textContent = movie.overview;
@@ -61,8 +59,12 @@ function createMarkup(movie) {
   <div class ="upcoming-section-wrap">
   
     <picture class="upcoming-poster">
-        <source media="(min-width:768px)" srcset="https://image.tmdb.org/t/p/original${movie.backdrop_path}">
-        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="poster_path" style="width:auto;">
+        <source media="(min-width:768px)" srcset="https://image.tmdb.org/t/p/original${
+          movie.backdrop_path
+        }">
+        <img src="https://image.tmdb.org/t/p/w500${
+          movie.poster_path
+        }" alt="poster_path" style="width:auto;">
     </picture>
 
     <div>
@@ -71,31 +73,40 @@ function createMarkup(movie) {
         <div class ="info-film">
             <div class="info-film-block">
               <div class="info-film-item">
-                <p class="info-film-item--text">Release date</p>
-                <p class="info-film--resp-release-date">${(movie.release_date).replace(/^(\d+)-(\d+)-(\d+)$/, `$3.$2.$1`)}</p>
+                <p class="info-film-item-text">Release date</p>
+                <p class="info-film-resp-release-date">${movie.release_date.replace(
+                  /^(\d+)-(\d+)-(\d+)$/,
+                  `$3.$2.$1`
+                )}</p>
               </div>
 
               <div class="info-film-item">
-                <p class="info-film-item--text">Vote / Votes</p>
-                <p class="info-film--votes-count"><span>${movie.vote_average}</span><b>/</b><span>${movie.vote_count}</span></p>
+                <p class="info-film-item-text">Vote / Votes</p>
+                <p class="info-film--votes-count"><span>${
+                  movie.vote_average
+                }</span><b>/</b><span>${movie.vote_count}</span></p>
               </div>
             </div>
             
             <div class="info-film-block">
               <div class="info-film-item">
-                <p class="info-film-item--text">Popularity</p>
-                <p class="info-film--popularity text-color-white">${(Number(movie.popularity.toFixed(1)))}</p>
+                <p class="info-film-item-text">Popularity</p>
+                <p class="info-film--popularity text-color-white">${Number(
+                  movie.popularity.toFixed(1)
+                )}</p>
               </div>
 
               <div class="info-film-item">
-                <p class="info-film-item--text">Genre</p>
-                <p class="info-film--genre-style text-color-white genre-padding-bottom">${movie.genres}</p>
+                <p class="info-film-item-text">Genre</p>
+                <p class="info-film--genre-style text-color-white genre-padding-bottom">${
+                  movie.genres
+                }</p>
               </div>
             </div> 
              
         </div>
     
-        <div class ="info-film--overview">
+        <div class ="info-film-overview">
       <h3 class="film-description-title">ABOUT</h3>
       <p class="film-description">${textContent}</p>
       <button class="btn">add to my library</button>
@@ -114,8 +125,8 @@ function onPageLoad() {
   });
 }
 
-
-{/* <div>
+{
+  /* <div>
         <h3 class="upcoming-film-title">${movie.title}</h3>
         
         <div class ="info-film">
@@ -153,4 +164,5 @@ function onPageLoad() {
   </div>`;
 
 
- */}
+ */
+}
