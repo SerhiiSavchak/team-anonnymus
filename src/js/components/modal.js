8b2b336206e3fd1dc9bc667b626a259998555364
IrinaBelenko <118209690+IrinaBelenko@users.noreply.github.com>
2023-06-12 23:18:53 +0300

    fix
    сделала экспортную функцию для вызова модального окна
    добавила слухачи подий в weekly and hero
    закоментила все консоли

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -1,11 +1,7 @@
-import { weeklyUlRef } from '../refs/modalRefs.js';
-
 import { createModalMarkup } from '../markup/modalMarkup.js';
 
-weeklyUlRef.addEventListener('click', selectMovie);
-
-function selectMovie(event) {
-  if (event.target.nodeName !== 'LI') {
+export function selectMovie(event) {
+  if (event.target.nodeName !== 'LI' && event.target.nodeName !== 'BUTTON') {
     return;
   }
 
