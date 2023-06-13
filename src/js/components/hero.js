ba4e35f2c96d5689a26c7f59adb787267692e4a9
X-RAY <intelekt200012@gmail.com>
2023-06-13 21:32:45 +0300

    add fix hero

--- src/js/components/hero.js
+++ src/js/components/hero.js
@@ -1,4 +1,3 @@
-//console.log('hello');
 import axios from 'axios';
 import Vimeo from '@vimeo/player';
 import { selectMovie } from './modal';
@@ -17,34 +16,77 @@ const options = {
   },
 };
 
-const heroSection = document.querySelector('.hero-section');
+let intervalId = null;
+const heroSectionRef = document.querySelector('.hero-section');
 const heroCont = document.querySelector('.hero-container');
-const btnClose = document.querySelector('.hero-modal-close');
-const backdropModal = document.querySelector('.hero-backdrop');
+const btnCloseRef = document.querySelector('.hero-modal-close');
+const backdropModalRef = document.querySelector('.hero-backdrop');
+const modalWrapRef = document.querySelector('.modal-wrap');
+
 window.addEventListener('load', onPageLoad);
 
-btnClose.addEventListener('click', onBtnCloseClick);
+btnCloseRef.addEventListener('click', onBtnCloseClick);
 
-function onBtnCloseClick(evt) {
-  backdropModal.classList.remove('.visuality-hidden');
-}
+// LISTENERS
 
 function onPageLoad() {
   fetchTranding(BASE_URL, END_POINT, options).then(res => {
     const markup = createMarkup(res);
-    addMarkup(markup);
+    addMarkup(heroCont, markup);
+    const heroBtnTrailer = document.querySelector('.js-open-video');
+    iFrameRef = document.querySelector('.iframe-hero');
+    heroBtnTrailer.addEventListener('click', onBtnOpenClick);
   });
 }
 
-async function fetchTranding(BASE_URL, END_POINT, options) {
-  const response = await fetch(`${BASE_URL}${END_POINT}`, options);
-  const data = await response.json();
-
-  return data.results[Math.floor(Math.random() * (20 - 1 + 1)) + 1];
+function onBtnCloseClick(evt) {
+  backdropModalRef.classList.add('visuality-hidden');
 }
 
+function onBtnOpenClick(evt) {
+  backdropModalRef.classList.remove('visuality-hidden');
+  console.log(evt.target.dataset.id);
+
+  fetchVideo(evt.target.dataset.id, options).then(res => {
+    const markup = createVideoMarkup(res);
+    console.log(markup);
+
+    addMarkup(modalWrapRef, markup);
+  });
+}
+
+// FETCH
+
+async function fetchTranding(BASE_URL, END_POINT, options) {
+  try {
+    const response = await fetch(`${BASE_URL}${END_POINT}`, options);
+    const data = await response.json();
+
+    return data.results[Math.floor(Math.random() * (20 - 1 + 1)) + 1];
+  } catch (error) {
+    console.log(error.message);
+    stopInterval(intervalId);
+  }
+}
+
+async function fetchVideo(id, options) {
+  try {
+    const response = await fetch(
+      `
+  ${BASE_URL}movie/${id}/videos`,
+      options
+    );
+    const data = await response.json();
+
+    return data.results[0].key;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// MARKUP
 function createMarkup(data) {
-  heroSection.style.backgroundImage = `linear-gradient(
+  heroSectionRef.style.backgroundImage = `linear-gradient(
     86.77deg, #111111 30.38%, rgba(17, 17, 17, 0) 65.61%), url("https://image.tmdb.org/t/p/original/${data.backdrop_path}")`;
 
   return `  <h1 class="hero-title-resp">${data.title}</h1>
@@ -54,11 +96,18 @@ function createMarkup(data) {
   <p class="hero-text-big-resp">
     ${data.overview.split('').slice(0, 225).join('') + '...'}
   </p>
-  <a href="" class="hero-btn-resp btn link">Watch trailer</a><a href="" class="hero-btn-black  link" data-id="${
+  <button type="button" class="hero-btn-resp js-open-video" data-id="${
     data.id
-  }">More details</a>`;
+  }" >Watch trailer</button> <button type="button"  class="hero-btn-black link" data-id="${
+    data.id
+  }">More details</button>`;
 }
 
-function addMarkup(markup) {
-  heroCont.innerHTML = markup;
+function createVideoMarkup(key) {
+  const videoUrl = `https://www.youtube.com/embed/${key}`;
+  return `<iframe  class="iframe-hero" width="250" height="150"  src="${videoUrl}"></iframe>`;
+}
+
+function addMarkup(element, markup) {
+  element.innerHTML = markup;
 }
