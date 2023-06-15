899e5b7471975f770b0cd5b91a71c4e84d3fb833
X-RAY <intelekt200012@gmail.com>
2023-06-15 05:58:27 +0300

    add fix theme

--- src/js/utils/theme.js
+++ src/js/utils/theme.js
@@ -2,17 +2,22 @@ const bodyStyle = document.querySelector('body');
 const themSwitcherButton = document.querySelector('.button-switch');
 const halfofmoon = document.querySelector('.icon-halfofmoon');
 const moonLeft = document.querySelector('.icon-moon-left');
+
 const moonRight = document.querySelector('.icon-moon-right');
 const iconSun = document.querySelector('.icon-sun');
 
 const saveMode = localStorage.getItem('selectedMode');
 
-if (saveMode === 'dark') {
-  bodyStyle.classList = 'dark';
-  halfofmoon.classList.add('visuality-hidden');
-  moonRight.classList.add('visuality-hidden');
-  moonLeft.classList.remove('visuality-hidden');
-  iconSun.classList.remove('visuality-hidden');
+window.addEventListener('load', onPageLoad);
+
+function onPageLoad() {
+  if (saveMode === 'light') {
+    bodyStyle.classList = saveMode;
+    halfofmoon.classList.add('visuality-hidden');
+    moonRight.classList.add('visuality-hidden');
+    moonLeft.classList.remove('visuality-hidden');
+    iconSun.classList.remove('visuality-hidden');
+  }
 }
 
 themSwitcherButton.addEventListener('click', switchThem);
@@ -25,10 +30,12 @@ function switchThem(event) {
 
   if (!iconSun.classList.contains('visuality-hidden')) {
     bodyStyle.classList.remove('dark');
+    bodyStyle.classList.add('light');
 
-    localStorage.setItem('selectedMode', 'dark');
-  } else {
-    bodyStyle.classList.add('dark');
     localStorage.setItem('selectedMode', 'light');
+  } else {
+    bodyStyle.classList.remove('light');
+    bodyStyle.classList.add('dark');
+    localStorage.setItem('selectedMode', 'dark');
   }
 }
